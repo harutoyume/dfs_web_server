43bd365db9fc13969eb88e7ba1aed6c1f66e56ae
Timur Salakhov <claymix@yandex.ru>
2025-04-25 22:23:58 +0300

    Added .env to repository. Added .env loading to Config

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
+from dotenv import load_dotenv
+
+# Check if there's .env file
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    # Load environment variables
+    load_dotenv(dotenv_path)
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
-    METADATA_SERVER_URL = os.environ.get('METADATA_SERVER_URL', 'http://localhost:8080')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-key')
+    METADATA_SERVER_URL = os.getenv('METADATA_SERVER_URL', 'http://localhost:8080')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB limit
